863b475a53f41114dbd1db4271c713100e5c68cf
Juan David Osorio Calderon <david.osorio@live.com>
2025-02-04 15:42:35 -0500

    Add Stater Number

--- Sales.API/Controllers/CountriesController.cs
+++ Sales.API/Controllers/CountriesController.cs
@@ -19,8 +19,18 @@ namespace Sales.API.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAsync()
         {
-            var countries = await _context.Countries.ToListAsync();
-            return Ok(countries);
+            return Ok(await _context.Countries
+                .Include(x => x.States)
+                .ToListAsync());
+        }
+
+        [HttpGet("full")]
+        public async Task<IActionResult> GetFullAsync()
+        {
+            return Ok(await _context.Countries
+                .Include(x => x.States!)
+                .ThenInclude(x => x.Cities)
+                .ToListAsync());
         }
 
         [HttpGet("{id:int}")]

4a0f5fada1c1244c04fa9db8ae6ddc6d94443832
Juan David Osorio Calderon <david.osorio@live.com>
2025-02-06 15:01:44 -0500

    Seguridad

--- Sales.Shared/Entities/City.cs
+++ Sales.Shared/Entities/City.cs
@@ -19,5 +19,8 @@ namespace Sales.Shared.Entities
         public int StateId { get; set; }
 
         public State? State { get; set; }
+
+        public ICollection<User>? Users { get; set; }
+
     }
 }

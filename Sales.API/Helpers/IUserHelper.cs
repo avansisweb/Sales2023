74f93d2d4dfcd687794adbd2c8f85a3f25f2a049
Juan David Osorio Calderon <david.osorio@live.com>
2025-02-08 11:29:49 -0500

    Mejora Paginación

--- Sales.API/Helpers/IUserHelper.cs
+++ Sales.API/Helpers/IUserHelper.cs
@@ -30,5 +30,8 @@ namespace Sales.API.Helpers
 
         Task<IdentityResult> ConfirmEmailAsync(User user, string token);
 
+        Task<string> GeneratePasswordResetTokenAsync(User user);
+
+        Task<IdentityResult> ResetPasswordAsync(User user, string token, string password);
     }
 }
